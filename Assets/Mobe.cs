fd343985abbae64999ecdb76f86e31afc739f8c5
Douggernort <Dougchug123@gmail.com>
2020-01-24 21:11:51 +0000

    yeah

---
+++ Assets/Mobe.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobe : MonoBehaviour
+{
+    public KeyCode forwardkey;
+    public KeyCode backwardkey;
+    public KeyCode turnleftkey;
+    public KeyCode turnrightkey;
+    public Rigidbody rb;
+    public float thrust = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, -75f * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 75f * Time.deltaTime, 0f); 
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(6f, 0f * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.W)) 
+        {
+            transform.Rotate(-6f, 0f * Time.deltaTime, 0f);
+        }
+    }
+}
---
+++ Assets/follow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Rotate(0f, 0f * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            transform.Rotate(0f, 0f * Time.deltaTime, 0f);
+        }
+    }
+}
