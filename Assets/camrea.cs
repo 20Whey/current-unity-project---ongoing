f7cc0657deaadcfb12fb2bb8cc4e58a1cbef2a72
Douggernort <Dougchug123@gmail.com>
2020-01-28 15:42:12 +0000

    phew

---
+++ Assets/camrea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camrea : MonoBehaviour
+{
+    private Vector3 cameratarget;
+    private Transform target;
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("player").transform;
+    }
+    void Update()
+    {
+        cameratarget = new Vector3(target.position.x, transform.position.y, target.position.z);
+        transform.position = Vector3.Lerp(transform.position, cameratarget, Time.deltaTime * 8);
+    }
+}
---
+++ Destroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public double hp;
+    public float maxHp;
+    public static List<Destroy> allDestructables = new List<Destroy>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allDestructables.Add(this);
+    }
+
+    // Update is called once per frame
+
+
+    void OnDestroy()
+    {
+        allDestructables.Remove(this);
+    }
+    public void TakeDmg()
+    {
+        hp = hp = 25f;
+        if (hp <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
